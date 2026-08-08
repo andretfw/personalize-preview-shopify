08726700912e44103151515dfb7624f2beda5527
Andreia <118443112+andretfw@users.noreply.github.com>
2026-08-09 01:15:01 +0300

    Return to single-side preview rendering

--- extensions/product-customizer/assets/product-customizer.js
+++ extensions/product-customizer/assets/product-customizer.js
@@ -30,9 +30,6 @@
         .then(() =>
           import(getAssetScriptUrl("product-customizer-controller-v2.js")),
         )
-        .then(() =>
-          import(getAssetScriptUrl("product-customizer-dual-preview.js")),
-        )
         .then(() => {
           loaded = true;
         })
