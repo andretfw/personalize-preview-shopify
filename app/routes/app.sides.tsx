6cdfcb80108a095dcf5fa34974fff064d740b068
Andreia <118443112+andretfw@users.noreply.github.com>
2026-08-09 16:23:34 +0300

    Hide internal surcharge products from print sides

--- app/routes/app.sides.tsx
+++ app/routes/app.sides.tsx
@@ -151,7 +151,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const response = await admin.graphql(
     `#graphql
       query PersonalizePreviewPrintSides {
-        products(first: 100, sortKey: TITLE) {
+        products(first: 100, sortKey: TITLE, query: "tag_not:personalize-preview-internal") {
           nodes {
             id
             title
